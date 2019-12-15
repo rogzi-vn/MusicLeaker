afdb968f7497ad94ce1b4dfd35b6360d9782199f
unknown <Vistark@NGHIA.tcu.edu.vn>
2019-12-15 14:43:51 +0700

    Finish Profile

--- front-end/src/api/User.js
+++ front-end/src/api/User.js
@@ -14,9 +14,29 @@ export async function getUser(username, password) {
 export async function getAvatar(email) {
     return await JAxios({
         url: '/get_user_avatar.php',
-        method: 'POST',
+        method: 'GET',
         params: {
             email
         }
     });
+}
+
+export async function updateSearchCount(userID) {
+    return await JAxios({
+        url: '/update_search_count.php',
+        method: 'GET',
+        params: {
+            id: userID
+        }
+    });
+}
+
+export async function getCountUserSong(username) {
+    return await JAxios({
+        url: '/count_user_song.php',
+        method: 'POST',
+        params: {
+            username
+        }
+    })
 }
--- front-end/src/layout/AppHeader.vue
+++ front-end/src/layout/AppHeader.vue
@@ -1,71 +1,65 @@
 <template>
-    <header class="header-global">
-        <base-nav class="navbar-main" transparent type="" effect="light" expand>
-            <router-link slot="brand" class="navbar-brand mr-lg-5" to="/">
-                <img src="img/brand/white.png" alt="logo">
-            </router-link>
+  <header class="header-global">
+    <base-nav class="navbar-main" transparent type="" effect="light" expand>
+      <router-link slot="brand" class="navbar-brand mr-lg-5" to="/">
+        <img src="img/brand/white.png" alt="logo" />
+      </router-link>
 
-            <div class="row" slot="content-header" slot-scope="{closeMenu}">
-                <div class="col-6 collapse-brand">
-                    <a href="https://demos.creative-tim.com/vue-argon-design-system/documentation/">
-                        <img src="img/brand/blue.png">
-                    </a>
-                </div>
-                <div class="col-6 collapse-close">
-                    <close-button @click="closeMenu"></close-button>
-                </div>
-            </div>
+      <div class="row" slot="content-header" slot-scope="{ closeMenu }">
+        <div class="col-6 collapse-brand">
+          <a
+            href="https://demos.creative-tim.com/vue-argon-design-system/documentation/"
+          >
+            <img src="img/brand/blue.png" />
+          </a>
+        </div>
+        <div class="col-6 collapse-close">
+          <close-button @click="closeMenu"></close-button>
+        </div>
+      </div>
 
-            <ul class="navbar-nav navbar-nav-hover align-items-lg-center">
-                <base-dropdown class="nav-item" menu-classes="dropdown-menu-xl">
-                    <a slot="title" href="#" class="nav-link" data-toggle="dropdown" role="button">
-                        <i class="ni ni-ui-04 d-lg-none"></i>
-                        <span class="nav-link-inner--text">Components</span>
-                    </a>
-                    <div class="dropdown-menu-inner">
-                        <a href="https://demos.creative-tim.com/vue-argon-design-system/documentation/"
-                           class="media d-flex align-items-center">
-                            <div class="icon icon-shape bg-gradient-primary rounded-circle text-white">
-                                <i class="ni ni-spaceship"></i>
-                            </div>
-                            <div class="media-body ml-3">
-                                <h6 class="heading text-primary mb-md-1">Getting started</h6>
-                                <p class="description d-none d-md-inline-block mb-0">Get started with Bootstrap, the
-                                    world's most popular framework for building responsive sites.</p>
-                            </div>
-                        </a>
-                        <a href="https://demos.creative-tim.com/vue-argon-design-system/documentation/"
-                           class="media d-flex align-items-center">
-                            <div class="icon icon-shape bg-gradient-warning rounded-circle text-white">
-                                <i class="ni ni-ui-04"></i>
-                            </div>
-                            <div class="media-body ml-3">
-                                <h5 class="heading text-warning mb-md-1">Components</h5>
-                                <p class="description d-none d-md-inline-block mb-0">Learn how to use Argon
-                                    compiling Scss, change brand colors and more.</p>
-                            </div>
-                        </a>
-                    </div>
-                </base-dropdown>
-            </ul>
-            <ul class="navbar-nav align-items-lg-center ml-lg-auto">
-                <li class="nav-item">
-                    <a class="nav-link nav-link-icon" href="https://www.facebook.com/tx.trongnghia98" target="_blank" rel="noopener"
-                       data-toggle="tooltip" title="Like us on Facebook">
-                        <i class="fa fa-facebook-square"></i>
-                        <span class="nav-link-inner--text d-lg-none">Facebook</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nav-link-icon" href="https://github.com/futureskyprojects"
-                       target="_blank" rel="noopener" data-toggle="tooltip" title="Star us on Github">
-                        <i class="fa fa-github"></i>
-                        <span class="nav-link-inner--text d-lg-none">Github</span>
-                    </a>
-                </li>
-            </ul>
-        </base-nav>
-    </header>
+      <ul class="navbar-nav navbar-nav-hover align-items-lg-center">
+        <div
+          slot="title"
+          href="#"
+          class="nav-link"
+          data-toggle="dropdown"
+          role="button"
+        >
+          <i class="ni ni-ui-04 d-lg-none"></i>
+          <span class="nav-link-inner--text">Tìm và lưu trữ nhạc của bạn</span>
+        </div>
+      </ul>
+      <ul class="navbar-nav align-items-lg-center ml-lg-auto">
+        <li class="nav-item">
+          <a
+            class="nav-link nav-link-icon"
+            href="https://www.facebook.com/tx.trongnghia98"
+            target="_blank"
+            rel="noopener"
+            data-toggle="tooltip"
+            title="Like us on Facebook"
+          >
+            <i class="fa fa-facebook-square"></i>
+            <span class="nav-link-inner--text d-lg-none">Facebook</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a
+            class="nav-link nav-link-icon"
+            href="https://github.com/futureskyprojects"
+            target="_blank"
+            rel="noopener"
+            data-toggle="tooltip"
+            title="Star us on Github"
+          >
+            <i class="fa fa-github"></i>
+            <span class="nav-link-inner--text d-lg-none">Github</span>
+          </a>
+        </li>
+      </ul>
+    </base-nav>
+  </header>
 </template>
 <script>
 import BaseNav from "@/components/BaseNav";
@@ -80,5 +74,4 @@ export default {
   }
 };
 </script>
-<style>
-</style>
+<style></style>
