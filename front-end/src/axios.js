6c9c65906b5982b130a2675b19d772d076bbe495
mchoang98 <40058856+mchoang98@users.noreply.github.com>
2019-12-24 14:42:56 +0700

    update resource

--- front-end/src/axios.js
+++ front-end/src/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 var JAxios = axios.create({ // Không có dấu '/' ở cuối
-  baseURL: 'http://musicleaker.unaux.com/api',
-  //baseURL: 'http://localhost/musicleaker/back-end/api', // <---------------------------
+ // baseURL: 'http://musicleaker.unaux.com/api',
+  baseURL: 'http://localhost/musicleaker/back-end/api', // <---------------------------
   timeout: 120000,
   headers: {
     Accept: "*/*",
